4601c98142b8bd5d4c0337f0b0f8029ce9db7786
Matt Foyle <mfoyle93@gmail.com>
2024-10-19 11:50:35 -0700

    fix: exports from route

--- app/api/fund-wallet/route.ts
+++ app/api/fund-wallet/route.ts
@@ -3,18 +3,6 @@ import { Address, createWalletClient, http, parseEther } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { storyTestnet } from 'viem/chains'
 
-export const walletClient = createWalletClient({
-  chain: storyTestnet,
-  transport: http(),
-})
-
-// Handle both 0x-prefixed and non-prefixed private keys
-const privateKeyHex = process.env.PRIVATE_KEY?.startsWith('0x')
-  ? process.env.PRIVATE_KEY
-  : `0x${process.env.PRIVATE_KEY}`
-
-export const account = privateKeyToAccount(privateKeyHex as `0x${string}`)
-
 export async function POST(req: NextRequest) {
   // Parse the request body
   const body = await req.json()
@@ -22,6 +10,19 @@ export async function POST(req: NextRequest) {
 
   console.log('Received wallet address:', walletAddress)
   console.log('Funding amount:', amount)
+
+  const walletClient = createWalletClient({
+    chain: storyTestnet,
+    transport: http(),
+  })
+  
+  // Handle both 0x-prefixed and non-prefixed private keys
+  const privateKeyHex = process.env.PRIVATE_KEY?.startsWith('0x')
+    ? process.env.PRIVATE_KEY
+    : `0x${process.env.PRIVATE_KEY}`
+  
+  const account = privateKeyToAccount(privateKeyHex as `0x${string}`)
+  
 
   if (!walletAddress || !amount) {
     return NextResponse.json({ message: 'Wallet address and amount are required' }, { status: 400 })
